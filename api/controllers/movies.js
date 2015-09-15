6b8ec5210b443df90ecdbc730efd69821c7b4730
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-09-15 14:48:43 +0100

    Working on the concept

--- api/controllers/movies.js
+++ api/controllers/movies.js
@@ -4,19 +4,26 @@ var MovieDB = require('moviedb')('0bb137d978545e9b6314278018a36c59');
 var getMovies = function(req,res){
  // API Key
  // MovieDB.searchMulti({query: 'Indiana Jones'}, function(err, result){
-var query = [];
+  var myquery = [];
   MovieDB.searchMulti({query: req.query.query }, function(err, result){
     if (err) {
- 
+
       res.status(404).send(err);
     }
     res.status(200).send(result.results);
     
-    for (var i = 0, len = result.results.length; i < len; i++) {
-        query = result.results[i].id;
-    }
-    console.log(query)
+    // for (var i = 0, len = result.results.length; i < len; i++) {
+    //   myquery = result.results[i].id;
+    //   MovieDB.movieVideos({id: myquery}, function(err, video){
+
+    //     res.status(200).send(video.results,result.results );
+    //     console.log(video.results)
+    //   });
+    // }
+
+    console.log(myquery)
   })
+
 
 }
 module.exports = {
--- frontend/js/controllers/moviesController.js
+++ frontend/js/controllers/moviesController.js
@@ -7,15 +7,22 @@ angular
       $sceProvider.enabled(false);
     });
 
-MovieController.$inject = ["$resource"];
+MovieController.$inject = ["$resource", '$filter'];
 
-function MovieController($resource){
-
+function MovieController($resource, $filter){
+  var orderBy = $filter('orderBy');
   var self = this;
   self.text = ""
   self.movie_id = [];
   self.allMovies=[];
+  self.predicate = '-title';
 
+  self.reverse = true;
+  self.order = function(predicate) {
+    self.reverse = (self.predicate === predicate) ? !self.reverse : false;
+    self.predicate = predicate;
+
+  }
   var Movie = $resource("http://localhost:3000/api/movies/:id",
     { id: '@_id' }, 
     { "search": {
@@ -35,11 +42,9 @@ function MovieController($resource){
      }, function(result){
       self.allMovies = result
       console.log(result)
-        self.predicate = 'title';
-              self.reverse = true;
-              self.order = function(predicate) {
-                self.reverse = (self.predicate === predicate) ? !self.reverse : false;
-                self.predicate = predicate;
+      for (i=0; i < result.length; i++){
+        var test = result[i].genre_id
+        console.log(test)
       }
     }
     )
@@ -49,4 +54,4 @@ function MovieController($resource){
     }  
   }
   
- }
+}
