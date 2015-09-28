d2b4ca27e15d313d666176e49bd9f77fa6379003
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-09-28 14:48:14 +0100

    fixing root issue on sugnup and login

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(passport.initialize());
 app.get('/api/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
 
 app.get('/api/auth/facebook/callback', passport.authenticate('facebook',{
-  successRedirect: 'http://localhost:3000/ ',
-  failureRedirect: 'http://localhost:3000/login'
+  successRedirect: './',
+  failureRedirect: './login'  
   })
 )
 
